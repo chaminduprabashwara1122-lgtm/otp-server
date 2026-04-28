3e8b2afd26cd318806ff5590420630b20054e2ae
chaminduprabashwara1122-lgtm <chaminduprabashwara1122@gmail.com>
2026-04-28 11:13:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const app = express();
 
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("OTP Server Running 🚀");
+});
+
+// Subscription URL
+app.get("/subscribe", (req, res) => {
+  res.send("Subscription successful");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
